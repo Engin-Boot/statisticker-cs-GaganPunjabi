4642c518503e7544aa13659b13ab79167f4c5fcb
Gagan <gagan.punjabi@philips.com>
2020-09-03 17:12:44 +0530

    Added unit test

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,31 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        public Stats CalculateStatistics(List<double> numbers)
+        {
             //Implement statistics here
+            Stats calculator = new Stats();
+            if (numbers.Count > 0)
+            {
+                var sum = 0.0;
+                calculator.max = numbers[0];
+                calculator.min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    sum += number;
+                    if (number > calculator.max)
+                        calculator.max = number;
+                    if (number < calculator.min)
+                        calculator.min = number;
+                }
+                Console.WriteLine(calculator.max);
+                calculator.average = sum / numbers.Count;
+            }
+            else
+            {
+                calculator.max = calculator.min = calculator.average = double.NaN;
+            }
+            return calculator;
         }
     }
 }
